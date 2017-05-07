7963d0850cf94081d4e9b3a13ff8e215b759617f
NoahSekreter <nsekreter@gmail.com>
2017-05-07 15:04:18 -0400

    Added migration

--- ReviewProject/Models/Review.cs
+++ ReviewProject/Models/Review.cs
@@ -12,10 +12,11 @@ namespace ReviewProject.Models
         [Key]
         public int ID { get; set; }
 
-        [Display(Name = "Location")]
+        [Display(Name = "Destination")]
         public string Title { get; set; }
 
         public string Content { get; set; }
+        public string Author { get; set; }
 
         [Display(Name = "Date")] [DisplayFormat(DataFormatString = "{0:dd MMMM yyyy}")]
         public DateTime PublishedDate { get; set; }
